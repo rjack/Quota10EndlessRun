03a207eaca66455539a2a9d64d779cec18b75850
Gittorio <vittoriovernassa@gmail.com>
2026-01-30 11:17:42 +0100

    Added DoTween

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    [SerializeField]
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    
+    
+    
+}
