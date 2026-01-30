2cdb4447c420272c28745dad0a02d1341136d034
Gittorio <vittoriovernassa@gmail.com>
2026-01-30 11:42:25 +0100

    Main Menu First iteration

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,20 +7,22 @@ public class MainMenu : MonoBehaviour
 {
 
     [SerializeField] private CanvasGroup cv;
+    [SerializeField] private GameObject optionsMenu;
     
     public void StartGame()
     {
         cv.DOFade(1f,0.8f).SetEase(Ease.OutQuad).OnComplete(() => SceneManager.LoadScene(1));
     }
 
-    
+    public void OpenOptions()
+    {
+        optionsMenu.SetActive(true);
+    }
     
     public void QuitGame()
     {
         Application.Quit();
     }
-    
-    
-    
-    
+
+
 }
