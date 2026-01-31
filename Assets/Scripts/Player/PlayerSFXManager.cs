75b24a367daa179ae962d9be3c432898a07fefba
Roberto.menna <robertomenna.mo@gmail.com>
2026-01-31 23:31:51 +0100

    update walk

--- Assets/Scripts/Player/PlayerSFXManager.cs
+++ Assets/Scripts/Player/PlayerSFXManager.cs
@@ -2,16 +2,25 @@ using UnityEngine;
 
 public class PlayerSFXManager : MonoBehaviour
 {
-    private AudioSource audioSource;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioSource audioSourceWalk;
 
     [SerializeField] private AudioClip[] walkSFX;
     [SerializeField] private AudioClip jumpSFX;
     [SerializeField] private AudioClip[] hurtSFX;
 
+    private float counterWalkSFX = 0f;
+    private float delayWalkSFX = 0.45f;
 
-    void Awake()
+    private void Update()
     {
-        audioSource = GetComponent<AudioSource>();
+        // Fai partire il suoni di camminata ogni delayWalkSFX secondi
+        counterWalkSFX += Time.deltaTime;
+        if (counterWalkSFX >= delayWalkSFX)
+        {
+            PlayWalkSFX();
+            counterWalkSFX = 0f;
+        }
     }
 
     public void PlayWalkSFX()
